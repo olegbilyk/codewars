1c41c03326e7e2d487c27173bb5657b9d50391a8
olegbilyk <oleg.bilik1994@gmail.com>
2019-02-20 17:14:24 +0200

    FIX: 3 kyu - Text align justify

--- katas/3-kyu/Text align justify.js
+++ katas/3-kyu/Text align justify.js
@@ -107,62 +107,83 @@ function justify(string = '', length = 30) {
 
       result += `${line}\n`;
       index = length + 1;
+    } else if (index === 1) {
+      str = str.replace(/^\S+/, (match = '') => {
+        result += `${match}`;
+        return '';
+      });
     }
   }
 
   return result.slice(0, -1);
 }
 
+const demo =
+  'Lorem   ipsum  dolor\n' +
+  'sit            amet,\n' +
+  'consectetur\n' +
+  'adipiscing     elit.\n' +
+  'Vestibulum  sagittis\n' +
+  'dolor   mauris,   at\n' +
+  'elementum     ligula\n' +
+  'tempor eget. In quis\n' +
+  'rhoncus   nunc,   at\n' +
+  'aliquet  orci. Fusce\n' +
+  'at  dolor  sit  amet\n' +
+  'felis       suscipit\n' +
+  'tristique.   Nam   a\n' +
+  'imperdiet    tellus.\n' +
+  'Nulla  eu vestibulum\n' +
+  'urna.        Vivamus\n' +
+  'tincidunt   suscipit\n' +
+  'enim,  nec  ultrices\n' +
+  'nisi   volutpat  ac.\n' +
+  'Maecenas   sit  amet\n' +
+  'lacinia   arcu,  non\n' +
+  'dictum  justo. Donec\n' +
+  'sed  quam  vel risus\n' +
+  'faucibus    euismod.\n' +
+  'Suspendisse  rhoncus\n' +
+  'rhoncus   felis   at\n' +
+  'fermentum.     Donec\n' +
+  'lorem         magna,\n' +
+  'ultricies a nunc sit\n' +
+  'amet,        blandit\n' +
+  'fringilla  nunc.  In\n' +
+  'vestibulum  velit ac\n' +
+  'felis        rhoncus\n' +
+  'pellentesque. Mauris\n' +
+  'at    tellus   enim.\n' +
+  'Aliquam     eleifend\n' +
+  'tempus      dapibus.\n' +
+  'Pellentesque\n' +
+  'commodo,   nisi  sit\n' +
+  'amet       hendrerit\n' +
+  'fringilla, ante odio\n' +
+  'porta    lacus,   ut\n' +
+  'elementum      justo\n' +
+  'nulla et dolor.';
+
+console.log(
+  'TEST =',
+  justify(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.',
+    20,
+  ) === demo,
+);
+
+console.log('----- DEMO -----', demo);
+
+console.log(
+  justify(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.',
+    1,
+  ),
+);
+
 console.log(
   justify(
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.',
     20,
   ),
-);
-
-console.log(
-  'Lorem   ipsum  dolor\n' +
-    'sit            amet,\n' +
-    'consectetur\n' +
-    'adipiscing     elit.\n' +
-    'Vestibulum  sagittis\n' +
-    'dolor   mauris,   at\n' +
-    'elementum     ligula\n' +
-    'tempor eget. In quis\n' +
-    'rhoncus   nunc,   at\n' +
-    'aliquet  orci. Fusce\n' +
-    'at  dolor  sit  amet\n' +
-    'felis       suscipit\n' +
-    'tristique.   Nam   a\n' +
-    'imperdiet    tellus.\n' +
-    'Nulla  eu vestibulum\n' +
-    'urna.        Vivamus\n' +
-    'tincidunt   suscipit\n' +
-    'enim,  nec  ultrices\n' +
-    'nisi   volutpat  ac.\n' +
-    'Maecenas   sit  amet\n' +
-    'lacinia   arcu,  non\n' +
-    'dictum  justo. Donec\n' +
-    'sed  quam  vel risus\n' +
-    'faucibus    euismod.\n' +
-    'Suspendisse  rhoncus\n' +
-    'rhoncus   felis   at\n' +
-    'fermentum.     Donec\n' +
-    'lorem         magna,\n' +
-    'ultricies a nunc sit\n' +
-    'amet,        blandit\n' +
-    'fringilla  nunc.  In\n' +
-    'vestibulum  velit ac\n' +
-    'felis        rhoncus\n' +
-    'pellentesque. Mauris\n' +
-    'at    tellus   enim.\n' +
-    'Aliquam     eleifend\n' +
-    'tempus      dapibus.\n' +
-    'Pellentesque\n' +
-    'commodo,   nisi  sit\n' +
-    'amet       hendrerit\n' +
-    'fringilla, ante odio\n' +
-    'porta    lacus,   ut\n' +
-    'elementum      justo\n' +
-    'nulla et dolor.',
 );
