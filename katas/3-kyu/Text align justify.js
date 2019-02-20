9384241c8d6ac998228f1535721825d5aef572da
olegbilyk <oleg.bilik1994@gmail.com>
2019-02-20 16:36:14 +0200

    DONE: 3 kyu - Text align justify

--- katas/3-kyu/Text align justify.js
+++ katas/3-kyu/Text align justify.js
@@ -66,11 +66,103 @@
 */
 
 /**
- * @param {String} str - inital string
- * @param {Number} len - line length
+ * @param {String} string - inital string
+ * @param {Number} length - line length
  *
  * @return {String}
  */
-const justify = function(str, len) {};
+function justify(string = '', length = 30) {
+  let result = '';
+  let str = string.replace(/\n/g, ' ');
 
-console.log(justify('Lorem ipsum dolor sit amet'), 'Lorem  ipsum  dolor  sit amet,');
+  for (let index = length; index >= 0; index--) {
+    if (str.length === 0) {
+      break;
+    }
+
+    if ([' ', ''].includes(str.charAt(index))) {
+      let line = '';
+      const currStr = str.slice(0, index);
+      str = str.slice(index + 1);
+
+      if (currStr.length === length || str.length === 0) {
+        line = currStr;
+      } else {
+        const words = currStr.split(' ');
+        let allSpaceLength = length - currStr.length + words.length - 1;
+
+        for (let wordIndex = 0, endWords = words.length; wordIndex < endWords; wordIndex++) {
+          let spaceCount = Math.ceil(allSpaceLength / (endWords - wordIndex - 1));
+          spaceCount = !Number.isNaN(spaceCount) && Number.isFinite(spaceCount) ? spaceCount : 0;
+
+          line += words[wordIndex] + ' '.repeat(spaceCount);
+
+          if (spaceCount > 0) {
+            allSpaceLength -= spaceCount;
+          } else {
+            break;
+          }
+        }
+      }
+
+      result += `${line}\n`;
+      index = length + 1;
+    }
+  }
+
+  return result.slice(0, -1);
+}
+
+console.log(
+  justify(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.',
+    20,
+  ),
+);
+
+console.log(
+  'Lorem   ipsum  dolor\n' +
+    'sit            amet,\n' +
+    'consectetur\n' +
+    'adipiscing     elit.\n' +
+    'Vestibulum  sagittis\n' +
+    'dolor   mauris,   at\n' +
+    'elementum     ligula\n' +
+    'tempor eget. In quis\n' +
+    'rhoncus   nunc,   at\n' +
+    'aliquet  orci. Fusce\n' +
+    'at  dolor  sit  amet\n' +
+    'felis       suscipit\n' +
+    'tristique.   Nam   a\n' +
+    'imperdiet    tellus.\n' +
+    'Nulla  eu vestibulum\n' +
+    'urna.        Vivamus\n' +
+    'tincidunt   suscipit\n' +
+    'enim,  nec  ultrices\n' +
+    'nisi   volutpat  ac.\n' +
+    'Maecenas   sit  amet\n' +
+    'lacinia   arcu,  non\n' +
+    'dictum  justo. Donec\n' +
+    'sed  quam  vel risus\n' +
+    'faucibus    euismod.\n' +
+    'Suspendisse  rhoncus\n' +
+    'rhoncus   felis   at\n' +
+    'fermentum.     Donec\n' +
+    'lorem         magna,\n' +
+    'ultricies a nunc sit\n' +
+    'amet,        blandit\n' +
+    'fringilla  nunc.  In\n' +
+    'vestibulum  velit ac\n' +
+    'felis        rhoncus\n' +
+    'pellentesque. Mauris\n' +
+    'at    tellus   enim.\n' +
+    'Aliquam     eleifend\n' +
+    'tempus      dapibus.\n' +
+    'Pellentesque\n' +
+    'commodo,   nisi  sit\n' +
+    'amet       hendrerit\n' +
+    'fringilla, ante odio\n' +
+    'porta    lacus,   ut\n' +
+    'elementum      justo\n' +
+    'nulla et dolor.',
+);
